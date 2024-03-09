eeda45e801d4fca41bf0f63b370506bff414f856
J. LaFleur <47275243+jdlflr@users.noreply.github.com>
2024-03-09 12:14:01 -0500

    README docstrings and in-line comments

--- src/saqe/__init__.py
+++ src/saqe/__init__.py
@@ -33,24 +33,48 @@ class NounPhraseExtractor(object):
             A list of noun phrases extracted from the input text.
         """
         raw_noun_phrases = TextBlob(text).noun_phrases + [i.text for i in self._spacy_parser(text).noun_chunks]
+        # filter out uncased phrases
         raw_noun_phrases = [i for i in raw_noun_phrases if i in text]
+        # strip off leading stopwords and deduplicate the list of phrases
         noun_phrases = list(set([self._strip_off_leading_stop_words(noun_phrase) for noun_phrase in raw_noun_phrases]))
+        # sort phrases
         noun_phrases.sort()
+
         return noun_phrases
 
     @staticmethod
     def _strip_off_leading_stop_words(noun_phrase: str) -> str:
+        """
+        Strips off leading stopwords from a noun phrase.
+        Args:
+            noun_phrase: [REQUIRED] the noun phrase to process
+
+        Returns:
+            The noun phrase free of any leading stopwords.
+        """
         split_noun_phrase = noun_phrase.split()
-        if split_noun_phrase[0].lower() in stopwords.words():
+        while split_noun_phrase[0].lower() in stopwords.words():
             split_noun_phrase = split_noun_phrase[1:]
         return " ".join(split_noun_phrase)
 
 
 class TextSimilarity(object):
+    """
+    A class for computing semantic similarity between a piece of text (query) and a list of texts (corpus).
+    """
     def __init__(self, model_name: str):
         self._model = HuggingFaceEmbeddings(model_name=model_name, model_kwargs={'device': DEVICE})
 
     def similarity(self, query: str, corpus: List[str]) -> List[float]:
+        """
+        Computes semantic similarity between a piece of text (query) and a list of texts (corpus).
+        Args:
+            query: the text to compare against all texts in the corpus.
+            corpus: the comparator texts.
+
+        Returns:
+            List of computed cosine similarity values. They are index matched with the corpus texts.
+        """
         corpus_embeddings = self._model.embed_documents(corpus)
         query_embedding = [self._model.embed_query(query)]
         return cosine_similarity(query_embedding, corpus_embeddings)
